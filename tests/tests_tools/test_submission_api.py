ff7d38b880cadec3b14bed311ea0159a3f4dc37e
VianneyMI <vianney.mixtur@outlook.fr>
2025-04-29 00:17:43 +0200

    Add note

--- tests/tests_tools/test_submission_api.py
+++ tests/tests_tools/test_submission_api.py
@@ -5,6 +5,7 @@ import pytest
 from azrock.tools.submission_api import get_file_by_task_id
 
 
+# NOTE: To generalize, the agents might need to be able to work with other type of files like video, PDF, zip, web pages (html code).
 class TestGetFileByTaskId:
     """Tests for the get_file_by_task_id function."""
 
@@ -13,7 +14,6 @@ class TestGetFileByTaskId:
     TASK_ID_WITH_CODE = "f918266a-b3e0-4914-865d-4faa564f1aef"
     TASK_ID_WITH_SPREADSHEET = "7bd855d8-463d-4ed5-93ca-5fe35145f733"
 
-    @pytest.mark.non_deterministic
     def test_get_image(self):
         """Test the get_image function."""
 
