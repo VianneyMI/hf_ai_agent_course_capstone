c0d4eb8d2f216ca1eb96209aa09919a80e1b7530
VianneyMI <vianney.mixtur@outlook.fr>
2025-04-30 23:22:52 +0200

    WIP Adding tools to read files

---
+++ src/azrock/tools/file_reading.py
@@ -0,0 +1,71 @@
+"""`azrock.tools.file_reading` contains functions for reading files."""
+
+from io import BytesIO
+from PIL import Image
+from pydub import AudioSegment
+import pandas as pd
+from smolagents import tool
+
+IMAGE_FORMATS = [
+    "jpeg",
+    "jpg",
+    "png",
+    "gif",
+    "bmp",
+]
+
+
+# File reading API
+# ------------------------------------------------------------------------------
+def read_image(image_bytes: bytes) -> Image.Image:
+    """Reads an image from bytes.
+
+    Args:
+        image_bytes: bytes
+            The bytes of the image to read.
+
+    Returns:
+        Image.Image: The image data as a PIL Image object.
+    """
+
+    img = Image.open(BytesIO(image_bytes))
+    img.verify()  # Verify that the image is valid
+    img = Image.open(BytesIO(image_bytes))  # Reopen for further operations
+    assert img.format and img.format.lower() in IMAGE_FORMATS
+
+    return Image.open(BytesIO(image_bytes))
+
+
+def read_audio(audio_bytes: bytes) -> AudioSegment:
+    """Reads an audio from bytes.
+
+    Args:
+        audio_bytes: bytes
+            The bytes of the audio to read.
+
+    Returns:
+        AudioSegment: The audio data as a pydub AudioSegment object.
+    """
+
+    return AudioSegment.from_mp3(audio_bytes)
+
+
+def read_excel(excel_bytes: bytes) -> pd.DataFrame:
+    """Reads an excel file from bytes.
+
+    Args:
+        excel_bytes: bytes
+            The bytes of the excel file to read.
+
+    Returns:
+        pd.DataFrame: The excel data as a pandas DataFrame.
+    """
+
+    return pd.read_excel(BytesIO(excel_bytes))
+
+
+# Toolification
+# ------------------------------------------------------------------------------
+read_image_tool = tool(read_image)
+read_audio_tool = tool(read_audio)
+read_excel_tool = tool(read_excel)
