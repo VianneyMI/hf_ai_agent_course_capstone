8489fade4893f690d3823568ded102d96fa670f3
VianneyMI <vianney.mixtur@outlook.fr>
2025-05-01 18:13:57 +0200

    Add debug test on test_litellm

--- tests/test_miscellaneous/test_litellm.py
+++ tests/test_miscellaneous/test_litellm.py
@@ -1,6 +1,7 @@
 """Test the Litellm model."""
 
 from smolagents import LiteLLMModel
+from azrock.agent import setup
 
 
 def test_litellm():
@@ -13,6 +14,26 @@ def test_litellm():
                 "role": "user",
                 "content": """what is the capital of France?
                 
+                Answer with a single word.
+                """,
+            }
+        ],
+    )
+    assert result.content == "Paris"
+
+
+def test_litellm_with_google():
+    """Test the Litellm model with Google."""
+
+    setup()
+    model_id = "gemini/gemini-2.5-flash-preview-04-17"
+    llm = LiteLLMModel(model_id=model_id)
+    result = llm(
+        [
+            {
+                "role": "user",
+                "content": """what is the capital of France?    
+    
                 Answer with a single word.
                 """,
             }
