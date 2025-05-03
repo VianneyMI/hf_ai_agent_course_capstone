476b95524a8c2d0805effd4204b1cf5611ccac41
VianneyMI <vianney.mixtur@outlook.fr>
2025-05-03 16:09:57 +0200

    WIP on speech to text

---
+++ tests/test_miscellaneous/test_speech_recognition.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import speech_recognition as sr
+
+
+path_to_test_file = Path(__file__).parents[2] / "data" / "downloads" / "test_audio.mp4"
+
+
+def recognize_speech_from_file(path_to_file: Path) -> str:
+    """
+    Recognize speech from a file.
+    """
+    recognizer = sr.Recognizer()
+    audio_file = sr.AudioFile(str(path_to_file))
+
+    with audio_file as source:
+        audio_data = recognizer.record(source)
+
+    text = recognizer.recognize_vosk(audio_data)
+
+    return text
+
+
+def test_recognize_speech_from_file():
+    text = recognize_speech_from_file(path_to_test_file)
+    assert isinstance(text, str), text
+
+
+if __name__ == "__main__":
+    test_recognize_speech_from_file()
+    print("Test passed")
